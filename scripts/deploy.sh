84d985e2bd7ed0e7ee4026faf8281bc73febeae8
fsevenm <ayubasw@gmail.com>
2019-01-07 08:37:06 +0800

    Change script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,7 @@
 
 # we are in circle ci server context
 # deploy backend first
-now logout
-rm ~/.now/config.json
-now login
+
 cd ~/repo/apps/api
 now -e NODE_ENV=production --token $NOW_TOKEN --public
 now alias --token=$NOW_TOKEN
